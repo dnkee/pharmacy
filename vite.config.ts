5dc1398c07b8dd5ddf8302b5d4c949937b13c4cf
dnkee <147556640+dnkee@users.noreply.github.com>
2025-04-28 10:58:47 +0200

    ds

--- vite.config.ts
+++ vite.config.ts
@@ -4,26 +4,21 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
-    sourcemap: true,
-    rollupOptions: {
-      output: {
-        manualChunks: undefined
-      }
-    }
+    emptyOutDir: true,
   },
   server: {
     port: 3000,
     proxy: {
       '/api': {
-        target: 'https://pharmacy-backend-cyq7.onrender.com',
+        target: 'https://pharmacy-request-backend.onrender.com',
         changeOrigin: true,
-        secure: true
+        secure: true,
       },
-    }
+    },
   },
   preview: {
     port: 3000,
